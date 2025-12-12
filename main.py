355f81266d98bb77735abd00fe353887dd49bf4a
Jason <jaygeorgedunn@gmail.com>
2025-12-12 00:11:44 -0800

    fzggfg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 from fastapi import FastAPI
+from pydantic import BaseModel
 from fastapi.responses import HTMLResponse
 from src.main.classes.connector.Connector import Connector
 from src.main.run import RunEngine
@@ -19,48 +20,42 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# @app.post("/api/start")
-# def start():
-#     ads = run.plugin.get_data()
-#     run.connector = Connector(ads)
-#     run.run_engine()
-#     return {"response": {"ads": ads, "ratings": run.plugin.interests, "seed": run.seed}}
 
-
-# @app.post("/api/process")
-# def process(input: Input):
-#     input = json.loads(input.message)
-#     clean_input(input)
-#     new_data = run.plugin.change_data(input)
-#     run.connector = Connector(new_data)
-#     run.run_engine()
-#     return {"response": {"ads": new_data, "ratings": run.plugin.interests, "seed": run.seed}}
+class Input(BaseModel):
+    message: str
 
 
 @app.post("/api/data")
-def get_sample_data():
-    data = {
-        "data": [
-            {"id": 1, "name": "Sample Item 1", "value": 100},
-            {"id": 2, "name": "Sample Item 2", "value": 200},
-            {"id": 3, "name": "Sample Item 3", "value": 300}
-        ],
-        "total": 3,
-        "timestamp": "2024-01-01T00:00:00Z"
-    }
+def start():
+    ads = run.plugin.get_data()
+    run.connector = Connector(ads)
+    run.run_engine()
+    return {"response": {"ads": ads, "ratings": run.plugin.interests, "seed": run.seed}}
 
-    return data
 
-    # return {
-    #     "statusCode": 200,
-    #     "headers": {
-    #         "Access-Control-Allow-Origin": "*",
-    #         "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-    #         "Access-Control-Allow-Headers": "Content-Type",
-    #     },
-    #     "body": json.dumps(data)
-    # }
+@app.post("/api/process")
+def process(input: Input):
+    input = json.loads(input.message)
+    clean_input(input)
+    new_data = run.plugin.change_data(input)
+    run.connector = Connector(new_data)
+    run.run_engine()
+    return {"response": {"ads": new_data, "ratings": run.plugin.interests, "seed": run.seed}}
 
+
+# @app.post("/api/data")
+# def get_sample_data():
+#     data = {
+#         "data": [
+#             {"id": 1, "name": "Sample Item 1", "value": 100},
+#             {"id": 2, "name": "Sample Item 2", "value": 200},
+#             {"id": 3, "name": "Sample Item 3", "value": 300}
+#         ],
+#         "total": 3,
+#         "timestamp": "2024-01-01T00:00:00Z"
+#     }
+
+#     return data
 
 
 @app.get("/")
@@ -70,3 +65,7 @@ def read_root():
     run.run_engine()
     return {"response": {"ads": ads, "ratings": run.plugin.interests, "seed": run.seed}}
 
+
+def clean_input(input):
+    for item in input.items():
+        input[item[0]] = int(item[1])
