f10bdb9eb14d9edc4519c669e97e5280a281660b
Jason <jaygeorgedunn@gmail.com>
2025-12-29 17:18:44 -0800

    change cors policy

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["http://stateshaper-lessons.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
